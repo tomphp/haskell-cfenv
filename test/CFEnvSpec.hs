828ee2fbb1e298905daa36ab1ba9ba0ff10ead79
Tom Oram <tom@codept.com>
2018-05-16 22:35:17 +0100

    Refactor: Fix indentation in the tests

--- test/CFEnvSpec.hs
+++ test/CFEnvSpec.hs
@@ -16,36 +16,36 @@ spec = do
         setEnv "VCAP_APPLICATION" "{}"
       )
       $ do
-      it "returns error if PORT is not set" $ do
-        unsetEnv "PORT"
-        app <- CFEnv.current
-        app `shouldBe` Left "PORT is not set."
+        it "returns error if PORT is not set" $ do
+          unsetEnv "PORT"
+          app <- CFEnv.current
+          app `shouldBe` Left "PORT is not set."
 
-      it "returns error if PORT is not valid number" $ do
-        setEnv "PORT" "abc"
-        app <- CFEnv.current
-        app `shouldBe` Left "PORT must be an integer, got 'abc'."
+        it "returns error if PORT is not valid number" $ do
+          setEnv "PORT" "abc"
+          app <- CFEnv.current
+          app `shouldBe` Left "PORT must be an integer, got 'abc'."
 
-      it "returns error if HOME is not set" $ do
-        unsetEnv "HOME"
-        app <- CFEnv.current
-        app `shouldBe` Left "HOME is not set."
+        it "returns error if HOME is not set" $ do
+          unsetEnv "HOME"
+          app <- CFEnv.current
+          app `shouldBe` Left "HOME is not set."
 
-      it "returns error if MEMORY_LIMIT is not set" $ do
-        unsetEnv "MEMORY_LIMIT"
-        app <- CFEnv.current
-        app `shouldBe` Left "MEMORY_LIMIT is not set."
+        it "returns error if MEMORY_LIMIT is not set" $ do
+          unsetEnv "MEMORY_LIMIT"
+          app <- CFEnv.current
+          app `shouldBe` Left "MEMORY_LIMIT is not set."
 
-      it "returns error if VCAP_APPLICATION is not set" $ do
-        unsetEnv "VCAP_APPLICATION"
-        app <- CFEnv.current
-        app `shouldBe` Left "VCAP_APPLICATION is not set."
+        it "returns error if VCAP_APPLICATION is not set" $ do
+          unsetEnv "VCAP_APPLICATION"
+          app <- CFEnv.current
+          app `shouldBe` Left "VCAP_APPLICATION is not set."
 
-      it "returns Application when the environment is correct" $ do
-        app <- CFEnv.current
+        it "returns Application when the environment is correct" $ do
+          app <- CFEnv.current
 
-        app `shouldBe` (Right CFEnv.Application
-                              { port = 9000
-                              , home = "/home/userZ"
-                              , memoryLimit = "256M"
-                              })
+          app `shouldBe` (Right CFEnv.Application
+                                { port = 9000
+                                , home = "/home/userZ"
+                                , memoryLimit = "256M"
+                                })
