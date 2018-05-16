cb271c4e2cc35a46774b45274cd61ad0557bf90f
Tom Oram <tom@codept.com>
2018-05-16 22:34:41 +0100

    Get value from PORT env var

--- src/CFEnv.hs
+++ src/CFEnv.hs
@@ -4,29 +4,38 @@ module CFEnv
     ) where
 
 import System.Environment (lookupEnv)
+import Text.Read (readMaybe)
 
 data Application = Application
-     { home :: String
+     { port :: Int
+     , home :: String
      , memoryLimit :: String
      } deriving (Eq, Show)
 
 current :: IO (Either String Application)
 current = do
+        port <- lookupEnv "PORT"
         home <- lookupEnv "HOME"
         memoryLimit <- lookupEnv "MEMORY_LIMIT"
         vcapApplication <- lookupEnv "VCAP_APPLICATION"
+
+        let portEither = case port of
+                                Just p -> maybeToEither ("PORT must be an integer, got '" ++ p ++ "'.") (readMaybe p)
+                                Nothing -> Left "PORT is not set."
 
         let homeEither = maybeToEither "HOME is not set." home
         let memoryLimitEither = maybeToEither "MEMORY_LIMIT is not set." memoryLimit
         let vcapApplicationEither = maybeToEither "VCAP_APPLICATION is not set." vcapApplication
 
-        return $ mkApplication <$> vcapApplicationEither
+        return $ mkApplication <$> portEither
                                <*> homeEither
                                <*> memoryLimitEither
+                               <*> vcapApplicationEither
 
-mkApplication :: a -> String -> String -> Application
-mkApplication _ home memoryLimit =
-    Application { home = home
+mkApplication :: Int -> String -> String -> a -> Application
+mkApplication port home memoryLimit _ =
+    Application { port = port
+                , home = home
                 , memoryLimit = memoryLimit
                 }
 
--- test/CFEnvSpec.hs
+++ test/CFEnvSpec.hs
@@ -10,11 +10,22 @@ spec = do
   describe "CFEnv" $ do
     describe "current" $ before
       (do
+        setEnv "PORT" "9000"
         setEnv "HOME" "/home/userZ"
         setEnv "MEMORY_LIMIT" "256M"
         setEnv "VCAP_APPLICATION" "{}"
       )
       $ do
+      it "returns error if PORT is not set" $ do
+        unsetEnv "PORT"
+        app <- CFEnv.current
+        app `shouldBe` Left "PORT is not set."
+
+      it "returns error if PORT is not valid number" $ do
+        setEnv "PORT" "abc"
+        app <- CFEnv.current
+        app `shouldBe` Left "PORT must be an integer, got 'abc'."
+
       it "returns error if HOME is not set" $ do
         unsetEnv "HOME"
         app <- CFEnv.current
@@ -30,10 +41,11 @@ spec = do
         app <- CFEnv.current
         app `shouldBe` Left "VCAP_APPLICATION is not set."
 
-      it "returns Application if VCAP_APPLICATION is set" $ do
+      it "returns Application when the environment is correct" $ do
         app <- CFEnv.current
 
         app `shouldBe` (Right CFEnv.Application
-                              { home = "/home/userZ"
+                              { port = 9000
+                              , home = "/home/userZ"
                               , memoryLimit = "256M"
                               })
