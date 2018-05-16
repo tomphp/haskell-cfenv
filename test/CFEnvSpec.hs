692396878da02f9067514671a4f6a9f5264e1869
Tom Oram <tom@codept.com>
2018-05-16 22:12:30 +0100

    Refactor: Use before hook in tests

--- test/CFEnvSpec.hs
+++ test/CFEnvSpec.hs
@@ -8,38 +8,29 @@ import CFEnv
 spec :: Spec
 spec = do
   describe "CFEnv" $ do
-    context "current" $ do
-      it "returns Nothing if HOME is not set" $ do
-        unsetEnv "HOME"
-        setEnv "MEMORY_LIMIT" "512M"
-        setEnv "VCAP_APPLICATION" "{}"
-
-        app <- CFEnv.current
-
-        app `shouldBe` Left "HOME is not set."
-
-      it "returns Nothing if MEMORY_LIMIT is not set" $ do
-        setEnv "HOME" "/home/userY"
-        unsetEnv "MEMORY_LIMIT"
-        setEnv "VCAP_APPLICATION" "{}"
-
-        app <- CFEnv.current
-
-        app `shouldBe` Left "MEMORY_LIMIT is not set."
-
-      it "returns Nothing if VCAP_APPLICATION is not set." $ do
-        setEnv "HOME" "/home/userX"
-        unsetEnv "VCAP_APPLICATION"
-
-        app <- CFEnv.current
-
-        app `shouldBe` Left "VCAP_APPLICATION is not set."
-
-      it "returns Application if VCAP_APPLICATION is set" $ do
+    describe "current" $ before
+      (do
         setEnv "HOME" "/home/userZ"
         setEnv "MEMORY_LIMIT" "256M"
         setEnv "VCAP_APPLICATION" "{}"
+      )
+      $ do
+      it "returns error if HOME is not set" $ do
+        unsetEnv "HOME"
+        app <- CFEnv.current
+        app `shouldBe` Left "HOME is not set."
 
+      it "returns error if MEMORY_LIMIT is not set" $ do
+        unsetEnv "MEMORY_LIMIT"
+        app <- CFEnv.current
+        app `shouldBe` Left "MEMORY_LIMIT is not set."
+
+      it "returns error if VCAP_APPLICATION is not set" $ do
+        unsetEnv "VCAP_APPLICATION"
+        app <- CFEnv.current
+        app `shouldBe` Left "VCAP_APPLICATION is not set."
+
+      it "returns Application if VCAP_APPLICATION is set" $ do
         app <- CFEnv.current
 
         app `shouldBe` (Right CFEnv.Application
