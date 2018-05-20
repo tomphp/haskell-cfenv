f50547e7aef3e354b55118b0a7697564f76e43d4
Tom Oram <tom@codept.com>
2018-05-20 16:18:36 +0100

    Refactor: Use 4 indents instead of 2 (as per style guide)

--- src/System/CloudFoundry/Environment.hs
+++ src/System/CloudFoundry/Environment.hs
@@ -4,12 +4,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module System.CloudFoundry.Environment
-  ( Application(..)
-  , Limits (..)
-  , Service (..)
-  , current
-  , isRunningOnCf
-  ) where
+    ( Application(..)
+    , Limits (..)
+    , Service (..)
+    , current
+    , isRunningOnCf
+    ) where
 
 import Control.Monad ((>=>), join)
 import Data.Char (isSpace)
@@ -24,77 +24,77 @@ import qualified Data.Aeson.Types as AT
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 data Application = Application
-  { appId :: String
-  , applicationUris :: [String]
-  , cfApi :: String
-  , home :: String
-  , host :: String
-  , instanceId :: String
-  , index :: Int
-  , limits :: Limits
-  , memoryLimit :: String
-  , appName :: String
-  , pwd :: String
-  , port :: Int
-  , services :: [Service]
-  , spaceId :: String
-  , spaceName :: String
-  , tmpDir :: String
-  , user :: String
-  , version :: String
-  } deriving (Eq, Show)
+    { appId :: String
+    , applicationUris :: [String]
+    , cfApi :: String
+    , home :: String
+    , host :: String
+    , instanceId :: String
+    , index :: Int
+    , limits :: Limits
+    , memoryLimit :: String
+    , appName :: String
+    , pwd :: String
+    , port :: Int
+    , services :: [Service]
+    , spaceId :: String
+    , spaceName :: String
+    , tmpDir :: String
+    , user :: String
+    , version :: String
+    } deriving (Eq, Show)
 
 data Limits = Limits
-  { disk :: Int
-  , fds :: Int
-  , mem :: Int
-  } deriving (Eq, Show, Generic)
+    { disk :: Int
+    , fds :: Int
+    , mem :: Int
+    } deriving (Eq, Show, Generic)
 
 instance FromJSON Limits
 
 data Service = Service
-  { name :: String
-  , label :: String
-  , tags :: [String]
-  , plan :: String
-  -- , credentials :: Map String ???
-  } deriving (Eq, Show, Generic)
+    { name :: String
+    , label :: String
+    , tags :: [String]
+    , plan :: String
+    -- , credentials :: Map String ???
+    } deriving (Eq, Show, Generic)
 
 instance FromJSON Service
 
 isRunningOnCf :: IO Bool
 isRunningOnCf = envHasValue "VCAP_APPLICATION"
-  where envHasValue = lookupEnv >=> return . fromMaybe False . fmap isEmpty
-        isEmpty = not . (==) "" . dropWhile isSpace
+    where envHasValue = lookupEnv >=> return . fromMaybe False . fmap isEmpty
+          isEmpty = not . (==) "" . dropWhile isSpace
 
 current :: IO (Either String Application)
 current =  do
-  home <- stringFromEnv "HOME"
-  memoryLimit <- stringFromEnv "MEMORY_LIMIT"
-  pwd <- stringFromEnv "PWD"
-  port <- numberFromEnv "PORT"
-  tmpDir <- stringFromEnv "TMPDIR"
-  user <- stringFromEnv "USER"
+    home <- stringFromEnv "HOME"
+    memoryLimit <- stringFromEnv "MEMORY_LIMIT"
+    pwd <- stringFromEnv "PWD"
+    port <- numberFromEnv "PORT"
+    tmpDir <- stringFromEnv "TMPDIR"
+    user <- stringFromEnv "USER"
 
-  vcapServices <- lookupEnv "VCAP_SERVICES"
-  let services = fmap decodeVcapServices vcapServices
+    vcapServices <- lookupEnv "VCAP_SERVICES"
+    let services = fmap decodeVcapServices vcapServices
 
-  let parser = vcapApplicationParser <$> (fromMaybe (Right []) services)
-                                     <*> home
-                                     <*> memoryLimit
-                                     <*> pwd
-                                     <*> port
-                                     <*> tmpDir
-                                     <*> user
+    let parser = vcapApplicationParser <$> (fromMaybe (Right []) services)
+                                       <*> home
+                                       <*> memoryLimit
+                                       <*> pwd
+                                       <*> port
+                                       <*> tmpDir
+                                       <*> user
 
-  vcapApplication <- stringFromEnv "VCAP_APPLICATION"
+    vcapApplication <- stringFromEnv "VCAP_APPLICATION"
 
-  return $ join (decodeVcapApplication <$> parser <*> vcapApplication)
+    return $ join (decodeVcapApplication <$> parser <*> vcapApplication)
 
 decodeVcapApplication :: (A.Value -> AT.Parser Application) -> String -> Either String Application
 decodeVcapApplication parser = addErrorPrefix . parseJson
-  where parseJson = (A.eitherDecode . BL.pack) >=> AT.parseEither parser
-        addErrorPrefix = mapLeft ("VCAP_APPLICATION " ++)
+    where parseJson = (A.eitherDecode . BL.pack) >=> AT.parseEither parser
+          addErrorPrefix = mapLeft ("VCAP_APPLICATION " ++)
 
 vcapApplicationParser :: [Service]
                       -> String
@@ -105,43 +105,43 @@ vcapApplicationParser :: [Service]
                       -> String
                       -> A.Value -> AT.Parser Application
 vcapApplicationParser services home memoryLimit pwd port tmpDir user =
-  A.withObject "Application" $ \o -> do
-    appId <- o .: "application_id"
-    applicationUris <- o .: "application_uris"
-    cfApi <- o .: "cf_api"
-    host <- o .: "host"
-    instanceId <- o .: "instance_id"
-    index <- o .: "instance_index"
-    limits <- o .: "limits"
-    appName <- o .: "name"
-    spaceId <- o .: "space_id"
-    spaceName <- o .: "space_name"
-    version <- o .: "version"
+    A.withObject "Application" $ \o -> do
+        appId <- o .: "application_id"
+        applicationUris <- o .: "application_uris"
+        cfApi <- o .: "cf_api"
+        host <- o .: "host"
+        instanceId <- o .: "instance_id"
+        index <- o .: "instance_index"
+        limits <- o .: "limits"
+        appName <- o .: "name"
+        spaceId <- o .: "space_id"
+        spaceName <- o .: "space_name"
+        version <- o .: "version"
 
-    return Application {..}
+        return Application {..}
 
 decodeVcapServices :: String -> Either String [Service]
 decodeVcapServices = A.eitherDecode . BL.pack
 
 stringFromEnv :: String -> IO (Either String String)
 stringFromEnv envName = do
-  value <- lookupEnv envName
-  return $ maybeToEither (envName ++ " is not set.") value
+    value <- lookupEnv envName
+    return $ maybeToEither (envName ++ " is not set.") value
 
 numberFromEnv :: String -> IO (Either String Int)
 numberFromEnv envName = do
-  string <- stringFromEnv envName
-  return $ string >>= numberOrError (\p -> envName ++ " must be an integer, got '" ++ p ++ "'.")
+    string <- stringFromEnv envName
+    return $ string >>= numberOrError (\p -> envName ++ " must be an integer, got '" ++ p ++ "'.")
 
 numberOrError :: (String -> String) -> String -> Either String Int
 numberOrError error value =
-  maybeToEither (error value) (readMaybe value)
+    maybeToEither (error value) (readMaybe value)
 
 maybeToEither :: e -> Maybe v -> Either e v
 maybeToEither error =
-  \case
-    Just value -> Right value
-    Nothing    -> Left error
+    \case
+        Just value -> Right value
+        Nothing    -> Left error
 
 mapLeft :: (e -> e1) -> Either e a -> Either e1 a
 mapLeft f (Left error)  = Left $ f error
--- test/System/CloudFoundry/EnvironmentSpec.hs
+++ test/System/CloudFoundry/EnvironmentSpec.hs
@@ -11,148 +11,149 @@ import qualified System.CloudFoundry.Environment as CfEnv
 
 vcapApplicationJson :: String
 vcapApplicationJson = [r|{
-	"instance_id": "abc_instance_id",
-	"application_id": "abc_application_id",
-  "application_uris": [
-    "haskell-test.cfapps.io"
-  ],
-	"instance_index": 100,
-	"name": "app_name",
-	"port": 1080,
-	"host": "app_host",
-	"version": "xxx_version",
-	"application_uris": ["appname.apps.foundation"],
-	"space_id": "abc_space_id",
-	"space_name": "development",
-	"cf_api": "https://api.sys.foundation",
-	"limits": {
-	  "disk": 1024,
-    "fds": 16384,
-    "mem": 2048
-	}
+    "instance_id": "abc_instance_id",
+    "application_id": "abc_application_id",
+    "application_uris": [
+        "haskell-test.cfapps.io"
+    ],
+    "instance_index": 100,
+    "name": "app_name",
+    "port": 1080,
+    "host": "app_host",
+    "version": "xxx_version",
+    "application_uris": ["appname.apps.foundation"],
+    "space_id": "abc_space_id",
+    "space_name": "development",
+    "cf_api": "https://api.sys.foundation",
+    "limits": {
+        "disk": 1024,
+        "fds": 16384,
+        "mem": 2048
+    }
 }|]
 
 vcapServices :: String
 vcapServices = [r|
-  [{
-    "name": "service_name",
-    "label": "service_label",
-    "tags": ["tag_a"],
-    "plan": "service_plan"
-  }]
+    [{
+        "name": "service_name",
+        "label": "service_label",
+        "tags": ["tag_a"],
+        "plan": "service_plan"
+    }]
 |]
+
+setEnvVars :: IO ()
+setEnvVars = do
+    setEnv "HOME" "/home/userZ"
+    setEnv "MEMORY_LIMIT" "256M"
+    setEnv "PORT" "9000"
+    setEnv "PWD" "/pwd"
+    setEnv "TMPDIR" "/tmpdir"
+    setEnv "USER" "tom"
+    setEnv "VCAP_APPLICATION" vcapApplicationJson
+    setEnv "VCAP_SERVICES" vcapServices
 
 spec :: Spec
 spec = do
-  describe "CfEnv" $ do
-    describe "isRunningOnCf" $ do
-      it "returns true if VCAP_APPLICATION has a value" $ do
-        setEnv "VCAP_APPLICATION" "{}"
-        runningOnCf <- CfEnv.isRunningOnCf
-        runningOnCf `shouldBe` True
+      describe "CfEnv" $ do
+            describe "isRunningOnCf" $ do
+                  it "returns true if VCAP_APPLICATION has a value" $ do
+                      setEnv "VCAP_APPLICATION" "{}"
+                      runningOnCf <- CfEnv.isRunningOnCf
+                      runningOnCf `shouldBe` True
 
-      it "returns true if VCAP_APPLICATION is not set" $ do
-        unsetEnv "VCAP_APPLICATION"
-        runningOnCf <- CfEnv.isRunningOnCf
-        runningOnCf `shouldBe` False
+                  it "returns true if VCAP_APPLICATION is not set" $ do
+                      unsetEnv "VCAP_APPLICATION"
+                      runningOnCf <- CfEnv.isRunningOnCf
+                      runningOnCf `shouldBe` False
 
-      it "returns true if VCAP_APPLICATION is empty" $ do
-        setEnv "VCAP_APPLICATION" ""
-        runningOnCf <- CfEnv.isRunningOnCf
-        runningOnCf `shouldBe` False
+                  it "returns true if VCAP_APPLICATION is empty" $ do
+                      setEnv "VCAP_APPLICATION" ""
+                      runningOnCf <- CfEnv.isRunningOnCf
+                      runningOnCf `shouldBe` False
 
-      it "returns true if VCAP_APPLICATION is only whitespace" $ do
-        setEnv "VCAP_APPLICATION" "    "
-        runningOnCf <- CfEnv.isRunningOnCf
-        runningOnCf `shouldBe` False
+                  it "returns true if VCAP_APPLICATION is only whitespace" $ do
+                      setEnv "VCAP_APPLICATION" "    "
+                      runningOnCf <- CfEnv.isRunningOnCf
+                      runningOnCf `shouldBe` False
 
-    describe "current" $ before
-      (do
-        setEnv "HOME" "/home/userZ"
-        setEnv "MEMORY_LIMIT" "256M"
-        setEnv "PORT" "9000"
-        setEnv "PWD" "/pwd"
-        setEnv "TMPDIR" "/tmpdir"
-        setEnv "USER" "tom"
-        setEnv "VCAP_APPLICATION" vcapApplicationJson
-      )
-      $ do
-        it "returns error if HOME is not set" $ do
-          unsetEnv "HOME"
-          app <- CfEnv.current
-          app `shouldBe` Left "HOME is not set."
+            describe "current" $ before setEnvVars $ do
+                it "returns error if HOME is not set" $ do
+                    unsetEnv "HOME"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "HOME is not set."
 
-        it "returns error if MEMORY_LIMIT is not set" $ do
-          unsetEnv "MEMORY_LIMIT"
-          app <- CfEnv.current
-          app `shouldBe` Left "MEMORY_LIMIT is not set."
+                it "returns error if MEMORY_LIMIT is not set" $ do
+                    unsetEnv "MEMORY_LIMIT"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "MEMORY_LIMIT is not set."
 
-        it "returns error if PWD is not set" $ do
-          unsetEnv "PWD"
-          app <- CfEnv.current
-          app `shouldBe` Left "PWD is not set."
+                it "returns error if PWD is not set" $ do
+                    unsetEnv "PWD"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "PWD is not set."
 
-        it "returns error if PORT is not set" $ do
-          unsetEnv "PORT"
-          app <- CfEnv.current
-          app `shouldBe` Left "PORT is not set."
+                it "returns error if PORT is not set" $ do
+                    unsetEnv "PORT"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "PORT is not set."
 
-        it "returns error if PORT is not valid number" $ do
-          setEnv "PORT" "abc"
-          app <- CfEnv.current
-          app `shouldBe` Left "PORT must be an integer, got 'abc'."
+                it "returns error if PORT is not valid number" $ do
+                    setEnv "PORT" "abc"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "PORT must be an integer, got 'abc'."
 
-        it "returns error if TMPDIR is not set" $ do
-          unsetEnv "TMPDIR"
-          app <- CfEnv.current
-          app `shouldBe` Left "TMPDIR is not set."
+                it "returns error if TMPDIR is not set" $ do
+                    unsetEnv "TMPDIR"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "TMPDIR is not set."
 
-        it "returns error if USER is not set" $ do
-          unsetEnv "USER"
-          app <- CfEnv.current
-          app `shouldBe` Left "USER is not set."
+                it "returns error if USER is not set" $ do
+                    unsetEnv "USER"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "USER is not set."
 
-        it "returns error if VCAP_APPLICATION is not set" $ do
-          unsetEnv "VCAP_APPLICATION"
-          app <- CfEnv.current
-          app `shouldBe` Left "VCAP_APPLICATION is not set."
+                it "returns error if VCAP_APPLICATION is not set" $ do
+                    unsetEnv "VCAP_APPLICATION"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "VCAP_APPLICATION is not set."
 
-        it "returns error if VCAP_APPLICATION bad JSON" $ do
-          setEnv "VCAP_APPLICATION" "not-json"
-          app <- CfEnv.current
-          app `shouldBe` Left "VCAP_APPLICATION Error in $: string"
+                it "returns error if VCAP_APPLICATION bad JSON" $ do
+                    setEnv "VCAP_APPLICATION" "not-json"
+                    app <- CfEnv.current
+                    app `shouldBe` Left "VCAP_APPLICATION Error in $: string"
 
-        it "returns Application when the environment is correct" $ do
-          setEnv "VCAP_SERVICES" vcapServices
-          app <- CfEnv.current
-          app `shouldBe` Right CfEnv.Application
-                                { CfEnv.appId = "abc_application_id"
-                                , CfEnv.applicationUris = ["haskell-test.cfapps.io"]
-                                , CfEnv.cfApi = "https://api.sys.foundation"
-                                , CfEnv.home = "/home/userZ"
-                                , CfEnv.host = "app_host"
-                                , CfEnv.instanceId = "abc_instance_id"
-                                , CfEnv.index = 100
-                                , CfEnv.memoryLimit = "256M"
-                                , CfEnv.appName = "app_name"
-                                , CfEnv.pwd = "/pwd"
-                                , CfEnv.port = 9000
-                                , CfEnv.tmpDir = "/tmpdir"
-                                , CfEnv.spaceId = "abc_space_id"
-                                , CfEnv.spaceName = "development"
-                                , CfEnv.user = "tom"
-                                , CfEnv.version = "xxx_version"
-                                , CfEnv.limits = CfEnv.Limits
-                                  { CfEnv.disk = 1024
-                                  , CfEnv.fds  = 16384
-                                  , CfEnv.mem  = 2048
-                                  }
-                                , CfEnv.services =
-                                  [ CfEnv.Service
-                                      { CfEnv.name = "service_name"
-                                      , CfEnv.label = "service_label"
-                                      , CfEnv.tags = ["tag_a"]
-                                      , CfEnv.plan = "service_plan"
+                it "returns Application when the environment is correct" $ do
+                    app <- CfEnv.current
+                    let expected = CfEnv.Application
+                                    { CfEnv.appId = "abc_application_id"
+                                    , CfEnv.applicationUris = ["haskell-test.cfapps.io"]
+                                    , CfEnv.cfApi = "https://api.sys.foundation"
+                                    , CfEnv.home = "/home/userZ"
+                                    , CfEnv.host = "app_host"
+                                    , CfEnv.instanceId = "abc_instance_id"
+                                    , CfEnv.index = 100
+                                    , CfEnv.memoryLimit = "256M"
+                                    , CfEnv.appName = "app_name"
+                                    , CfEnv.pwd = "/pwd"
+                                    , CfEnv.port = 9000
+                                    , CfEnv.tmpDir = "/tmpdir"
+                                    , CfEnv.spaceId = "abc_space_id"
+                                    , CfEnv.spaceName = "development"
+                                    , CfEnv.user = "tom"
+                                    , CfEnv.version = "xxx_version"
+                                    , CfEnv.limits = CfEnv.Limits
+                                      { CfEnv.disk = 1024
+                                      , CfEnv.fds  = 16384
+                                      , CfEnv.mem  = 2048
                                       }
-                                  ]
-                                }
+                                    , CfEnv.services =
+                                      [ CfEnv.Service
+                                          { CfEnv.name = "service_name"
+                                          , CfEnv.label = "service_label"
+                                          , CfEnv.tags = ["tag_a"]
+                                          , CfEnv.plan = "service_plan"
+                                          }
+                                      ]
+                                    }
+                    app `shouldBe` Right expected
